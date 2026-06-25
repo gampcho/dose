406b362dd3874ad7ae566ab64f5a480efdad223c
ForeverWeLearn <theanh11052006@gmail.com>
2026-06-26 01:50:42 +0700

    refactor

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import { Geist_Mono, Source_Sans_3, Public_Sans } from "next/font/google"
+
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
+
+const publicSansHeading = Public_Sans({
+  subsets: ["latin"],
+  variable: "--font-heading",
+})
+
+const sourceSans3 = Source_Sans_3({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        sourceSans3.variable,
+        publicSansHeading.variable,
+      )}
+    >
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  )
+}
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-    return (
-        <h1 className="text-4xl mt-24 font-bold text-center">Dose</h1>
-    );
-}
